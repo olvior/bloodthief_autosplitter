a6fdc88771442b12c905f76c6f188dafa3e7ec83
Olvior <olvioro@gmail.com>
2024-06-26 20:26:30 +0200

    Fixed issue with high cpu usage

--- wsock.py
+++ wsock.py
@@ -14,9 +14,8 @@ async def wshandle(request):
    
     async for msg in ws:
         if msg.type == web.WSMsgType.text:
-            await ws.send_str("Hello, {}".format(msg.data))
-        elif msg.type == web.WSMsgType.binary:
-            await ws.send_bytes(msg.data)
+            #print(msg)
+            pass
         elif msg.type == web.WSMsgType.close:
             break
     return ws
